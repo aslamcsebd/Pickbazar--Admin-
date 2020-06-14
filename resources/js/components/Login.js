db8db34eef1fe601fe36d1f1a137fcd630750737
shamrat1 <yshamrat@gmail.com>
2020-06-14 22:44:24 +0600

    admin dashboard dependencies and react setup

---
+++ resources/js/components/Login.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import axios from "axios";
+// import axios from 'axios'
+
+class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            response: [],
+            email: '',
+            password: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleEmail = this.handleEmail.bind(this);
+        this.handlePassword = this.handlePassword.bind(this);
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        axios.post('/api/login',[this.state.email,this.state.password]).then( response =>{
+            console.log(response.data);
+        })
+    }
+    handleEmail(e){
+        e.preventDefault();
+        this.setState({
+            email : e.target.value
+        });
+        console.log(this.state.email)
+    }
+    handlePassword(e){
+        e.preventDefault();
+        this.setState({
+            password : e.target.value
+        });
+        console.log(this.state.password)
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">
+                            Email address
+                        </label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                            placeholder="Enter email"
+                            onChange={this.handleEmail}
+                        />
+                        <small id="emailHelp" className="form-text text-muted">
+                            We'll never share your email with anyone else.
+                        </small>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Password</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="exampleInputPassword1"
+                            placeholder="Password"
+                            onChange={this.handlePassword}
+                        />
+                    </div>
+                    <div className="form-check">
+                        <input
+                            type="checkbox"
+                            className="form-check-input"
+                            id="exampleCheck1"
+                        />
+                        <label
+                            className="form-check-label"
+                            htmlFor="exampleCheck1"
+                        >
+                            Check me out
+                        </label>
+                    </div>
+                    <button
+                        onClick={this.handleSubmit}
+                        className="btn btn-primary"
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Login;
